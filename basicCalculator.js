aebb81da3184c313a05655b8b32a700a22b8ace2
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-03 15:26:05 +0600

    basic calcualtor

---
+++ basicCalculator.js
@@ -0,0 +1,9 @@
+let aa = parseInt(prompt("please enter a num"))
+
+let bb = parseInt(prompt("please enter another num"))
+if(aa !=null && bb!=null){
+    console.log("Addition Of two numbers "+ (aa+bb) )
+    console.log("Substraction Of two numbers "+(aa-bb))
+    console.log("Multiplication Of two numbers "+(aa*bb))
+    console.log("Division Of two numbers "+(aa/bb))
+}
--- index.js
+++ index.js
@@ -24,28 +24,39 @@ console.log(a+b)
 
 // // Functions 
 
-function roll(){
-    console.log("hello")
+// function roll(){
+//     console.log("hello")
+// }
+// roll();
+// // Functions with paramets:
+
+// function a(a){
+//     console.log(a)
+// }
+
+// a(8+8)
+
+// // Arrow Functions
+// var hello = () => {
+//   return "Hello World!";
+// }
+// console.log(hello())
+
+// // arrow functions with parameters
+
+// var hello_2 = (a) =>{
+//     return 7+a
+// }
+
+// console.log(hello_2(2+2))
+
+// Calculator in JavaScipt 
+let aa = parseInt(prompt("please enter a num"))
+
+let bb = parseInt(prompt("please enter another num"))
+if(aa !=null && bb!=null){
+    console.log("Addition Of two numbers "+ (aa+bb) )
+    console.log("Substraction Of two numbers "+(aa-bb))
+    console.log("Multiplication Of two numbers "+(aa*bb))
+    console.log("Division Of two numbers "+(aa/bb))
 }
-roll();
-// Functions with paramets:
-
-function a(a){
-    console.log(a)
-}
-
-a(8+8)
-
-// Arrow Functions
-var hello = () => {
-  return "Hello World!";
-}
-console.log(hello())
-
-// arrow functions with parameters
-
-var hello_2 = (a) =>{
-    return 7+a
-}
-
-console.log(hello_2(2+2))
