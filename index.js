618842712d8ae0c09eda2f01ae6e45fd757b2a09
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-03 13:11:10 +0600

    javascript basics

---
+++ index.js
@@ -0,0 +1,20 @@
+// Javascipt Basics
+
+/*
+1. Variable:
+var  a = 1
+var b = 2
+console.log(a+b)
+*/
+
+// Object 
+let person={
+    name:"Abdul",
+    age:30
+};
+person.age = 2000
+// Entering the elements
+console.log(person)
+
+// Arrays
+
