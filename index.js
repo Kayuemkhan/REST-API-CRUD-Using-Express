8dbddb844f5189ee0b19f6034124805ab51b38bb
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-04 18:49:06 +0600

    basics of nodejs - path, OS, fs, EvenetEmitter, CreateServer

--- index.js
+++ index.js
@@ -7,48 +7,48 @@ var b = 2
 console.log(a+b)
 */
 
-// // Object 
-// let person={
-//     name:"Abdul",
-//     age:30
-// };
-// person.age = 2000
-// // Entering the elements
-// console.log(person)
+// Object 
+let person={
+    name:"Abdul",
+    age:30
+};
+person.age = 2000
+// Entering the elements
+console.log(person)
 
-// // Arrays
+// Arrays
 
-// let selectedColors = ["red","blue"]
-// selectedColors[2] = "green"
-// console.log(selectedColors.length)
+let selectedColors = ["red","blue"]
+selectedColors[2] = "green"
+console.log(selectedColors.length)
 
-// // Functions 
+// Functions 
 
-// function roll(){
-//     console.log("hello")
-// }
-// roll();
-// // Functions with paramets:
+function roll(){
+    console.log("hello")
+}
+roll();
+// Functions with paramets:
 
-// function a(a){
-//     console.log(a)
-// }
+function a(a){
+    console.log(a)
+}
 
-// a(8+8)
+a(8+8)
 
-// // Arrow Functions
-// var hello = () => {
-//   return "Hello World!";
-// }
-// console.log(hello())
+// Arrow Functions
+var hello = () => {
+  return "Hello World!";
+}
+console.log(hello())
 
-// // arrow functions with parameters
+// arrow functions with parameters
 
-// var hello_2 = (a) =>{
-//     return 7+a
-// }
+var hello_2 = (a) =>{
+    return 7+a
+}
 
-// console.log(hello_2(2+2))
+console.log(hello_2(2+2))
 
 // Calculator in JavaScipt 
 let aa = parseInt(prompt("please enter a num"))
--- node-basics/app.js
+++ node-basics/app.js
@@ -1,4 +1,63 @@
-function sayhello(){
-    console.log("Hello")
-}
-sayhello()
+
+// const logger= require('./logger')
+
+// logger.log("message")
+
+// const path = require('path')
+
+// var pathObj = path.parse(__filename)
+
+// console.log(pathObj)
+
+
+// // Opearting System Data Fetch
+// const os = require('os')
+// var totalmemory = os.totalmem()
+// var freememory = os.freemem()
+// console.log(totalmemory)
+// console.log(freememory)
+// console.log(`Total Memory: ${os.totalmem}`)
+
+// // Read the files in the folder 
+// const fs = require('fs')
+
+// // const files =  fs.readdirSync('./');
+
+// // console.log(files)
+
+// // current directory files 
+
+// fs.readdir('./',function(err,files){
+//     if(err) console.log("Error",err)
+//     else console.log("result",files)
+// })
+
+// //Events 
+// // This is an class, so it can tell which it do
+// const EventEmitter = require('events')
+// // this is an object, the actual istance 
+// const emitter = new EventEmitter()
+// //Register a listener
+// emitter.on('messageLogged',function(arg){
+//     console.log('listener Called',arg)
+// })
+// // // Raise Event
+// // emitter.emit('messageLogged')
+// // Emitter with some arguments
+// emitter.emit('messageLogged', {id : 1, url:'http:..'})
+
+//http 
+const { Socket } = require('dgram');
+const http = require('http');
+const server = http.createServer( (req,res)=>{
+    if(req.url  === '/'){
+        res.write("What's up , Emon Brother... ");
+        res.end();
+    }
+});
+// server.on("connection", (socket) =>{
+//     console.log("New Connection")
+// })
+server.listen(3000);
+
+console.log("Listening on port 3000");
