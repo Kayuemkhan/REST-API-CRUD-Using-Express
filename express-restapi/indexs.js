19a573e9738aed0c0efcfe53d15bf7c9c1c6ce33
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-11 08:12:09 +0600

    POST data Manupulation getting JSON

--- express-restapi/indexs.js
+++ express-restapi/indexs.js
@@ -1,3 +1,6 @@
+// Using Joy for input Validation
+const joi = require('joi');
+
 const express = require('express');
 const app = express();
 const teammembers = [
@@ -6,31 +9,68 @@ const teammembers = [
     {id:3,name:'Emon'}
 
 ]
+
+//
+// exporting json, 
+ app.use (express.json());
+
+
 // get function has two arguments , first one is url , second one is endpoint
 app.get('/h',(req,res)=>{
     res.send("What's up? ");
 });
+
+
+// api get req and send an array
 app.get('/api/courses',(req,res)=>{
     res.send([1,2,3]);
 })
+
+
+// api get req and send back the given text as id
 app.get('/api/courses/:id',(req,res) =>{
     res.send(req.params.id)
 })
 
 
+// api get req and send the given year and month
 app.get('/api/posts/:year/:month',(req,res)=>{
     var details = "my name is emon"
     res.send(req.query);
 })
+
+
+// api get req and filter by id 
 app.get('/api/androidteams/:id',(req,res)=>{
     const mem = teammembers.find( c => c.id === parseInt(req.params.id));
     if(!mem) res.status(404).send("The member is not available in Adnroid team");
     res.send(mem)
 })
+
+
+// api req and send the array
 app.get('/api/androidteams/',(req,res)=>{
     
     res.send(teammembers)
 })
+
+
+// POST req , get a name and add it to database
+
+app.post('/api/androidteams',(req,res)=>{
+    if(!req.body.name || req.body.name.length <2){{
+        // 400 Bad Request
+        res.status(400).send('Name is Required and Should be minimum 3 Characters!');
+        return;
+    }}
+    
+    const members ={
+        id: teammembers.length +1,
+        name: req.body.name
+    };
+    teammembers.push(members);
+    res.send(teammembers)
+});
 
 
 const port = process.env.port ||3000;
